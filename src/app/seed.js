f381f89bfce4997546d3dc868cfb81b55408b76d
ardbrl <ardabirol@gmail.com>
2022-08-29 18:52:35 +0300

    CalendarUp

---
+++ src/app/seed.js
@@ -0,0 +1,13 @@
+export const seedData = [
+{
+id: 1,
+abbvTitle: 'Mon',
+fullTitle: 'Monday',
+events: [
+{ details: 'Get Groceries', edit: false },
+{ details: 'Carpool', edit: false }
+],
+active: true
+},
+// ...,
+}
